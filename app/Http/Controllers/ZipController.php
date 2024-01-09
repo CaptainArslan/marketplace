35f91df2ed15ac486247e552a40805d6f117e40a
CaptainArslan <mughalarslan996@gmail.com>
2024-01-09 18:11:45 +0500

    update function to move file in the storage path

--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers;
 
 use ZipArchive;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
 
 class ZipController extends Controller
 {
@@ -29,26 +26,22 @@ class ZipController extends Controller
     {
         $request->validate([
             'app_type' => 'required|string|in:live,beta',
-            'file' => 'required|mimes:zip|max:10240', // Adjust max file size as needed
+            'file' => 'required|mimes:zip', // Adjust max file size as needed
         ]);
-
         // Upload the file
         $uploadedFile = $request->file('file');
-
         // Save the uploaded file
-        $storagePath = 'uploads/';
-        $uploadedFilePath = $uploadedFile->storeAs($storagePath, $uploadedFile->getClientOriginalName(), 'public');
-        // Log::info($uploadedFilePath);
-        Log::info(storage_path("app/public/{$uploadedFilePath}"));
-
+        $storagePath = storage_path('app\public\uploads');
+        // $uploadedFilePath = $uploadedFile->storeAs($storagePath, $uploadedFile->getClientOriginalName(), 'public');
+        $uploadedFilePath = uploadFile($uploadedFile, $storagePath);
         // Check if the file has been saved successfully
         if (!$uploadedFilePath) {
             return back()->withNotify(['error', 'Failed to save the uploaded file.']);
         }
-
+        $uploadedFilePath = storage_path("app/public/uploads/{$uploadedFilePath}");
         // Now, read the file and extract it
         $zip = new ZipArchive;
-        if ($zip->open(storage_path("app/public/{$uploadedFilePath}")) === true) {
+        if ($zip->open($uploadedFilePath) === true) {
             if (env('APP_ENV') == 'production') {
                 $extractPath = ($request->app_type == 'live') ? '/home/marketplacejdfun/public_html' : '/home/marketplacejdfun/staging_app';
             } else {
@@ -56,18 +49,18 @@ class ZipController extends Controller
             }
 
             Log::info($extractPath);
-            // Extract to the specified directory
             $zip->extractTo($extractPath);
             $zip->close();
 
             // Delete the temporary uploaded zip file
-            Storage::disk('public')->delete($uploadedFilePath);
+            // Storage::disk('public')->delete($uploadedFilePath);
+            $notify[] = ['success', 'Zip file uploaded and extracted successfully.'];
 
-            return back()->withNotify(['success', 'Zip file uploaded and extracted successfully.']);
+            return back()->withNotify($notify);
         }
 
         // Delete the temporary uploaded zip file in case of extraction failure
-        Storage::disk('public')->delete($uploadedFilePath);
+        // Storage::disk('public')->delete($uploadedFilePath);
 
         return back()->withNotify(['error', 'Failed to extract the zip file.']);
     }
